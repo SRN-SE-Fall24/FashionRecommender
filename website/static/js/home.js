cc361fa762dec1e5ba042d1701741dc70b95ed49
Nishad Tardalkar <nishadtardalkar97@gmail.com>
2024-11-22 08:11:51 -0500

    query now showing on results page

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -1,37 +1,34 @@
 var formattedFormData = {};
-$(document).ready(function(){
-    $(".recoButton1").click(function(e){
+$(document).ready(function () {
+    $(".recoButton1").click(function (e) {
         var formData = $('#recoForm').serializeArray();
-        
-        for(var i = 0; i < formData.length; i++){
+
+        for (var i = 0; i < formData.length; i++) {
             formattedFormData[formData[i]["name"]] = formData[i]["value"];
         }
-        console.log(formattedFormData);
         formData = JSON.stringify(formattedFormData)
-        var occasionValue=formattedFormData["occasion"];
-        var cityValue=formattedFormData["city"]
-        localStorage.setItem("occasionVal",occasionValue);
-        localStorage.setItem("cityVal",cityValue);
+        var occasionValue = formattedFormData["occasion"];
+        var cityValue = formattedFormData["city"]
+        localStorage.setItem("occasionVal", occasionValue);
+        localStorage.setItem("cityVal", cityValue);
         // console.log(occasionVal)
         // console.log(cityVal)
-        console.log(formData);
         e.preventDefault();
         $.ajax({
             type: "POST",
             url: "/recommendations",
             data: formData,
-            success: function(data){
-                
+            success: function (data) {
                 var str = "";
-                for(var i = 0; i < data["links"].length; i++){
+                for (var i = 0; i < data["links"].length; i++) {
                     str += data["links"][i] + " || ";
                 }
-                console.log(data["links"]);
                 var redirectUrl = window.location.protocol + "//" + window.location.host + "/results?" + str;
                 location.href = redirectUrl;
+                window.sessionStorage.setItem('query', formData);
             },
             dataType: "json",
-            contentType : "application/json"
+            contentType: "application/json"
         });
         formData = JSON.stringify(formattedFormData)
         // $.ajax({
@@ -45,8 +42,8 @@ $(document).ready(function(){
         //     contentType : "application/json"
         // })
     });
-    $(".recoButton1").click(function(e){
-        var loader= document.getElementById( 'center' )
+    $(".recoButton1").click(function (e) {
+        var loader = document.getElementById('center')
         loader.style.display = '';
     });
 });
